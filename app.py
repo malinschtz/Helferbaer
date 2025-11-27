dd35e994ca9c31d5345e14676ee30cc7f3b2f5d0
malinschtz <malin.schuetz136@gmail.com>
2025-11-27 13:22:59 +0100

    HTML + CSS ausprobiert

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/test/')
+def test():
+    return render_template('test.html')
