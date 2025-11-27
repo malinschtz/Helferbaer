b1d138bd075f2ee2da07acba59bdef4d4aeae8cf
malinschtz <malin.schuetz136@gmail.com>
2025-11-27 16:59:46 +0100

    Anmelde+Registrieren Platzhalter Methoden

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-@app.route('/test/')
+@app.route('/test/', methods=["GET", "POST"])
 def test():
+    if request.method == "POST":
+        if "helfer" in request.form:
+            action = request.form["helfer"]
+            if action == "login":
+                return "Helfer Login ausgewählt"
+            elif action == "register":
+                return "Helfer Registrierung ausgewählt"
+
+        if "kunde" in request.form:
+            action = request.form["kunde"]
+            if action == "login":
+                return "Kunde Login ausgewählt"
+            elif action == "register":
+                return "Kunde Registrierung ausgewählt"
+    
     return render_template('test.html')
