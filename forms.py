609e2fccd0c975abb2fae4daba86dd43ea13ffd9
malinschtz <malin.schuetz136@gmail.com>
2026-01-07 12:42:16 +0100

    login und register forms erstellt

--- forms.py
+++ forms.py
@@ -1,4 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import InputRequired, Length
+from wtforms import StringField, PasswordField, DateField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
 
+class LoginForm(FlaskForm):
+    email = StringField('E-Mail', validators=[
+        DataRequired(), 
+        Email()
+    ])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    submit = SubmitField('Anmelden')
+
+class RegisterForm(FlaskForm):
+    firstName = StringField('Vorname', validators=[
+        DataRequired(), 
+        Length(min=2, max=50)
+    ])
+    name = StringField('Nachname', validators=[
+        DataRequired(), 
+        Length(min=2, max=50)
+    ])
+    birthday = DateField('Geburtsdatum', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[
+        DataRequired(), 
+        Email(),
+        Length(min=6, max=120)
+    ])
+    phone = StringField('Telefon', validators=[Optional()])
+    password = PasswordField('Passwort', validators=[
+        DataRequired(), 
+        Length(min=8)
+    ])
+    password_confirm = PasswordField('Wiederholen', validators=[
+        DataRequired(), 
+        EqualTo('password')
+    ])
+    submit = SubmitField('Registrieren')
