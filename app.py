d4c4d9bb1ea7a7944cbbcb738db383e9e5ab3ef1
malinschtz <malin.schuetz136@gmail.com>
2026-01-21 14:18:58 +0100

    Quellen eingefügt und kleine debugs

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from forms import LoginForm, RegisterForm, StellenangebotForm, JobFilterForm
-from sqlalchemy import select
+from sqlalchemy import select, func
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from flask_bcrypt import Bcrypt
 from datetime import date
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)    #Quelle: Flask-Bcrypt, Abschnitt "Usage"
+bcrypt = Bcrypt(app)    #Quelle: Flask-Bcrypt
 
 app.config.from_mapping(                                    
     SECRET_KEY = 'secret_key_just_for_dev_environment',     
@@ -26,10 +26,10 @@ def load_user(userId):
     return db.session.get(User, userId)     #Quelle: SQLAlchemy Session Basics, Abschnitt "Get by Primary Key"
 
 
-@app.route('/', methods=['GET', 'POST'])    #Quelle: Kursmaterial Basic routing
+@app.route('/', methods=['GET', 'POST'])    #Quelle: Kursmaterial Flask routing und requests, Abschnitt "5"
 def index():
     if request.method == 'POST':
-        if 'helfer' in request.form:
+        if 'helfer' in request.form:        #Quelle: Kursmaterial Flask routing und requests, Abschnit "6"
             action = request.form['helfer']
             if action == 'login':
                 return redirect(url_for('helfer_anmelden'))
@@ -54,7 +54,7 @@ def helfer():
     # Stundenkonto
     hours = current_user.current_month_hours_helfer
     DE_MONTHS = {1: 'Januar', 2: 'Februar', 3: 'März', 4: 'April',5: 'Mai', 6: 'Juni', 7: 'Juli', 8: 'August',9: 'September', 10: 'Oktober', 11: 'November', 12: 'Dezember'}
-    monat_name = f"{DE_MONTHS[date.today().month]} {date.today().year}"     #Quelle: Python datetime today
+    monat_name = f"{DE_MONTHS[date.today().month]} {date.today().year}"     #Quelle: Python datetime today, Abschnitt "date Objects"
 
     # Gebuchte und erledigte Jobs
     jobs_data = current_user.get_jobs_by_status_helfer()
@@ -63,18 +63,20 @@ def helfer():
 
 @app.route('/helfer/anmelden', methods=['GET', 'POST'])
 def helfer_anmelden():
-    form = LoginForm()
-    if form.validate_on_submit(): # Kombiniert if request.method == 'POST': & if form.validate():
-        user = User.query.filter_by(email=form.email.data).first()
+    form = LoginForm()      #Quelle: Kursmaterial User Interfaces, Abschnitt "2"
+    if form.validate_on_submit(): # Kombiniert if request.method == 'POST' & if form.validate() Quelle: Flask-WTF, Abschnitt "Validating Forms"
+        user = db.session.execute(      #Quelle: Kursmaterial SQLAlchemy, Abschnitt "7"
+            select(User).filter_by(email=form.email.data)
+            ).scalar_one_or_none()      
         if not user:
             flash('Email nicht gefunden. Bitte erst registrieren!', 'error')
-        elif not bcrypt.check_password_hash(user.password, form.password.data):
+        elif not bcrypt.check_password_hash(user.password, form.password.data):     #Quelle: Flask-Bcrypt
             flash('Passwort ungültig!', 'error')
         elif user.role != 'helfer':
             flash('Nur Alltagshelfer können sich hier anmelden. Bitte melden Sie sich als Kunde an', 'error')
         else: 
-            login_user(user)
-            flash('Willkommen zurück!', 'success')
+            login_user(user)        #Quelle: Flask-Login, Abschnitt "Login Example"
+            flash(f'Willkommen zurück {user.firstName}!', 'success')
             return redirect(url_for('helfer'))
     return render_template('helfer_anmelden.html', form=form)
 
@@ -83,7 +85,10 @@ def helfer_registrieren():
     form = RegisterForm()
     if form.validate_on_submit():
         #prüfen, ob email bereits vorhanden
-        if User.query.filter_by(email=form.email.data).first():
+        user = db.session.execute(
+            select(User).filter_by(email=form.email.data)
+            ).scalars().first()
+        if user:
             flash('Email breits registriert', 'error')
             return render_template('helfer_registrieren.html', form=form)
             
@@ -93,7 +98,7 @@ def helfer_registrieren():
             birthday = form.birthday.data,
             email = form.email.data,
             phone = form.phone.data or None,
-            password = bcrypt.generate_password_hash(form.password.data).decode('utf-8'),
+            password = bcrypt.generate_password_hash(form.password.data).decode('utf-8'),       #Quelle: Flask-Bcrypt, Abschnitt "Usage"
             role = 'helfer'
         )
 
@@ -108,27 +113,24 @@ def helfer_registrieren():
 @login_required
 def helfer_stellenangebot():
     form = JobFilterForm()
-    
-    # Dynamische Kategorien laden
-    categories = Category.query.all()
+
+    categories = db.session.execute(select(Category)).scalars()
     form.category.choices = [(0, 'Alle')] + [(c.catId, c.catName) for c in categories]
     
-    jobs = Job.query.filter(
-        Job.statusId == 1,
-        Job.helferId.is_(None) 
-    ).order_by(Job.date.asc())
-    
+    #db.session.execute(select(Model)) führt zu Problem wegen Rückgabe Objekt, Quelle: KI Prompi 1
+    stmt = select(Job).where(Job.statusId == 1).order_by(Job.date.asc())
+
     if form.validate_on_submit():
         if form.search.data:
-            jobs = jobs.filter(Job.description.ilike(f'%{form.search.data}%'))
+            stmt = stmt.where(Job.description.ilike(f'%{form.search.data}%'))
         if form.category.data != '0':
-            jobs = jobs.filter(Job.catId == form.category.data)
+            stmt = stmt.where(Job.catId == form.category.data)
         if form.plz.data:
-            jobs = jobs.filter(Job.plz == int(form.plz.data))
+            stmt = stmt.where(Job.plz == int(form.plz.data))
         if form.min_hours.data:
-            jobs = jobs.filter(Job.hours >= form.min_hours.data)
-    
-    jobs = jobs.paginate(page=request.args.get('page', 1, type=int), per_page=10)
+            stmt = stmt.where(Job.hours >= form.min_hours.data)
+        
+    jobs = db.paginate(stmt, page=request.args.get('page', 1, type=int), per_page=10)
     
     return render_template('helfer_stellenangebot.html', form=form, jobs=jobs)
 
@@ -145,23 +147,19 @@ def helfer_kunde_profil(kunde_id):
     
     kunde = db.session.get(User, kunde_id)
     # Alle gemeinsamen Jobs
-    kunde_jobs = Job.query.filter(
-        Job.helferId == current_user.userId,
-        Job.kundeId == kunde.userId,
-        Job.statusId == 3
-    ).order_by(Job.date.desc()).all()
+    kunde_jobs = db.session.execute(select(Job).filter_by(
+        helferId=current_user.userId,
+        kundeId=kunde.userId,
+        statusId=3
+        ).order_by(Job.date.desc())
+        ).scalars()
 
-    # Alle Jobs des Kunden
-    total_jobs = Job.query.filter(
-        Job.kundeId == kunde.userId,
-        Job.statusId == 3 
-    ).count()
-    return render_template('helfer_kunde_profil.html', kunde=kunde, kunde_jobs=kunde_jobs, total_jobs=total_jobs)
+    return render_template('helfer_kunde_profil.html', kunde=kunde, kunde_jobs=kunde_jobs)
 
 @app.route('/helfer/job_buchen/<int:job_id>', methods=['POST'])
 @login_required
 def helfer_job_buchen(job_id):
-    job = Job.query.get_or_404(job_id)
+    job = db.session.get(Job, job_id)
     job.helferId = current_user.userId
     job.statusId = 2 
     db.session.commit()
@@ -174,21 +172,22 @@ def kunde():
     if request.method == 'POST':
         return
 
-    else:
-        #Stundenkonto
-        hours = current_user.current_month_hours_kunde
-        DE_MONTHS = {1: 'Januar', 2: 'Februar', 3: 'März', 4: 'April',5: 'Mai', 6: 'Juni', 7: 'Juli', 8: 'August',9: 'September', 10: 'Oktober', 11: 'November', 12: 'Dezember'}
-        monat_name = f"{DE_MONTHS[date.today().month]} {date.today().year}"
+    #Stundenkonto
+    hours = current_user.current_month_hours_kunde
+    DE_MONTHS = {1: 'Januar', 2: 'Februar', 3: 'März', 4: 'April',5: 'Mai', 6: 'Juni', 7: 'Juli', 8: 'August',9: 'September', 10: 'Oktober', 11: 'November', 12: 'Dezember'}
+    monat_name = f"{DE_MONTHS[date.today().month]} {date.today().year}"
 
-        #Anfragen und Erledigte Jobs
-        jobs_data = current_user.get_jobs_by_status_kunde()
-        return render_template('kunde_startseite.html', hours=hours, monat_name=monat_name, **jobs_data)
+    #Anfragen und Erledigte Jobs
+    jobs_data = current_user.get_jobs_by_status_kunde()
+    return render_template('kunde_startseite.html', hours=hours, monat_name=monat_name, **jobs_data)
 
 @app.route('/kunde/anmelden', methods=['GET', 'POST'])
 def kunde_anmelden():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = db.session.execute(   
+            select(User).filter_by(email=form.email.data)
+            ).scalar_one_or_none()      
         if not user:
             flash('Email nicht gefunden. Bitte erst registrieren!', 'error')
         elif not bcrypt.check_password_hash(user.password, form.password.data):
@@ -197,7 +196,7 @@ def kunde_anmelden():
             flash('Nur Kunden können sich hier anmelden. Bitte melden Sie sich als Helfer an.', 'error')
         else: 
             login_user(user)
-            flash('Willkommen zurück!', 'success')
+            flash(f'Willkommen zurück {user.firstName}!', 'success')
             return redirect(url_for('kunde'))
     return render_template('kunde_anmelden.html', form=form)
 
@@ -206,7 +205,10 @@ def kunde_registrieren():
     form = RegisterForm()
     if form.validate_on_submit():
         #prüfen, ob email bereits vorhanden
-        if User.query.filter_by(email=form.email.data).first():
+        user = db.session.execute(
+            select(User).filter_by(email=form.email.data)
+            ).scalars().first()
+        if user:
             flash('Email breits registriert', 'error')
             return render_template('kunde_registrieren.html', form=form)
             
@@ -231,10 +233,7 @@ def kunde_registrieren():
 @login_required
 def kunde_stellenangebot():
     form = StellenangebotForm()
-    print(f"Request method: {request.method}")
-    print(f"Form valid: {form.validate_on_submit()}")
-    print(f"Form errors: {form.errors}")
-    print(f"Description data: {form.description.data}")
+
     if form.validate_on_submit():
         job = Job(
             kundeId = current_user.userId,
@@ -271,27 +270,28 @@ def kunde_helfer_profil(helfer_id):
     
     helfer = db.session.get(User, helfer_id)
     # Alle gemeinsamen Jobs
-    helfer_jobs = Job.query.filter(
-        Job.kundeId == current_user.userId,
-        Job.helferId == helfer.userId,
-        Job.statusId == 3
-    ).order_by(Job.date.desc()).all()
+    helfer_jobs = db.session.execute(select(Job).filter_by(
+        kundeId=current_user.userId,
+        helferId=helfer.userId,
+        statusId=3
+    ).order_by(Job.date.desc())
+    ).scalars()
 
     # Alle Jobs des Helfers 
-    total_jobs = Job.query.filter(
-        Job.helferId == helfer.userId,
-        Job.statusId == 3  # Erledigt
-    ).count()
+    total_jobs = db.session.execute(select(func.count(Job.jobId)).filter_by(    #Quelle: SQLAlchemy SQL and Generic Functions, Abschnitt "Selected “Known” Functions"
+        helferId=helfer.userId,
+        statusId=3
+    )).scalar()
     
     return render_template('kunde_helfer_profil.html', helfer=helfer, helfer_jobs=helfer_jobs, total_jobs=total_jobs)
 
 @app.route('/kunde/job/<int:job_id>/done', methods=['POST'])
 @login_required
 def kunde_job_erledigt(job_id):
-    job = Job.query.get_or_404(job_id)
+    job = job = db.session.get(Job, job_id)
     real_hours = request.form.get('real_hours')
-    if real_hours:
-        job.realHours = float(real_hours)
+    
+    job.realHours = float(real_hours)
     job.statusId = 3
     db.session.commit()
     flash('Job als erledigt markiert!', 'success')
@@ -300,7 +300,7 @@ def kunde_job_erledigt(job_id):
 @app.route('/logout')
 @login_required
 def logout():
-    logout_user()
+    logout_user()   #Quelle: Flask-Login, Abschnitt "Login Example"
     flash('Erfolgreich ausgeloggt.', 'info')
     return redirect(url_for('index'))
    
